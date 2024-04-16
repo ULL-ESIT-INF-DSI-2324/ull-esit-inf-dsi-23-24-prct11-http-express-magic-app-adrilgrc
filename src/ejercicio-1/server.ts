a55bcb35b37ad54c0cb662a3a3674a8a6abec7a0
Adrián Lima García <71635056+adrilgrc@users.noreply.github.com>
2024-04-16 22:24:04 +0000

    [U]: Add function working

--- src/ejercicio-1/server.ts
+++ src/ejercicio-1/server.ts
@@ -4,111 +4,53 @@ import { Card } from './card.js';
 import chalk from 'chalk';
 import { Color, TypeLine, Rarity } from './enums.js';
 import { ManagerServer } from './managerserver.js';
+import express from 'express';
 
 
 const cardAppInstance = new App();
-const managerServerInstance = new ManagerServer();
 
+const server = express();
 
-/**
- * Listener for the 'request' event
- */
-managerServerInstance.on('request', (request, connection) => {
-  console.log('[INFO]: Request received:', request);
-  let new_card: Card;
-  switch (request.command) {
-    case 'add':
-      // Create a new Card instance with the request data
-      new_card = new Card(
-        request.card._id,
-        request.card._name,
-        request.card._manaCost,
-        Color[request.card._color as keyof typeof Color],
-        TypeLine[request.card._typeLine as keyof typeof TypeLine],
-        Rarity[request.card._rarity as keyof typeof Rarity],
-        request.card._text,
-        request.card._value,
-        request.card._strength,
-        request.card._endurance,
-        request.card._loyaltyMark
-      );
-      // Then call addCard with the user, the new card and a callback
-      cardAppInstance.addCard(request.user, new_card, (err, result) => {
-        if (err) {
-          connection.write(chalk.red(`${err.message}`));
-        } else {
-          connection.write(chalk.green('[OK]: Card added successfully'));
-        }
-        connection.end();
-      });
-      break;
-    case 'remove':
-      // Call removeCard with the user, the card ID and a callback
-      cardAppInstance.removeCard(request.user, request.id, (err, result) => {
-        if (err) {
-          connection.write(chalk.red(`${err.message}`));
-        } else {
-          connection.write(chalk.green('[OK]: Card removed successfully'));
-        }
-        connection.end();
-      });
-      break;
-    case 'modify':
-      // Create a new Card instance with the request data
-      new_card = new Card(
-        request.card._id,
-        request.card._name,
-        request.card._manaCost,
-        Color[request.card._color as keyof typeof Color],
-        TypeLine[request.card._typeLine as keyof typeof TypeLine],
-        Rarity[request.card._rarity as keyof typeof Rarity],
-        request.card._text,
-        request.card._value,
-        request.card._strength,
-        request.card._endurance,
-        request.card._loyaltyMark
-      );
-      new_card = new Card(
-        request.card._id,
-        request.card._name,
-        request.card._manaCost,
-        Color[request.card._color as keyof typeof Color],
-        TypeLine[request.card._typeLine as keyof typeof TypeLine],
-        Rarity[request.card._rarity as keyof typeof Rarity],
-        request.card._text,
-        request.card._value,
-        request.card._strength,
-        request.card._endurance,
-        request.card._loyaltyMark
-      );
-      cardAppInstance.modifyCard(request.user, request.id, new_card, (error, result) => {
-        if (error) {
-          connection.write(chalk.red(`${error.message}`));
-        } else {
-          connection.write(chalk.green('[OK]: Card modified successfully'));
-        }
-        connection.end();
-      });
-      break;
-    case 'list':
-      // Call ListCards with the user and write the result to the connection
-      connection.write(cardAppInstance.ListCards(request.user));
-      connection.end();
-      break;
-    case 'show':
-      // Show card details
-      cardAppInstance.showCard(request.user, request.id, (err, cardInfo) => {
-        if (err) {
-          connection.write(chalk.red(`[ERROR]: ${err.message}`));
-        } else {
-          connection.write(cardInfo);
-        }
-        connection.end();
-      });
-      break;
-    default:
-      connection.write('[ERROR]: Invalid command');
-      connection.end();
-      break;
+server.use(express.json());
+
+server.get('/cards', (req, res) => {
+  if (!req.query.user) {
+    res.send({
+      status: '[ERROR]',
+      message: 'User is required',
+    });
+  return;
   }
+});
+
+server.post('/cards', (req, res) => {
+  if (!req.query.user) {
+    res.send({
+      status: '[ERROR]',
+      message: 'User is required',
+    });
+  return;
+  }
+  console.log(req.body);
+  //                new Card(argv.id, argv.name, argv.manaCost, Color[argv.color as keyof typeof Color], TypeLine[argv.typeLine as keyof typeof TypeLine] , Rarity[argv.rarity as keyof typeof Rarity], argv.text, argv.value, argv.strength, argv.endurance, argv.loyaltyMark);
+  const new_card = new Card(req.body.id as number, req.body.name as string, req.body.manaCost as number, Color[req.body.color as keyof typeof Color], TypeLine[req.body.typeLine as keyof typeof TypeLine], Rarity[req.body.rarity as keyof typeof Rarity], req.body.text as string, req.body.value as number, req.body.strength as number, req.body.endurance as number, req.body.loyaltyMark as number);
+  cardAppInstance.addCard(req.query.user as string, new_card, (error, result) => {
+    if (error) {
+      res.send({
+        status: '[ERROR]',
+        message: error,
+      });
+      return;
+    }
+    res.send({
+      status: '[OK]',
+      message: `Card with id '${req.body.id}' added to user '${req.query.user}'`,
+    });
+  });
+
+});
+
+
+server.listen(3000, () => {
+  console.log(chalk.green('[INFO]: Server listening on port 3000'));
 });
